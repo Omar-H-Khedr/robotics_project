1eab42a51af0bd62871399f4735306e27bfce174
Omar <omar.khedr@gu.edu.eg>
2026-05-25 20:08:54 +0300

    Add experiment configuration matrix for proposal simulation cell v1.10

--- ros2_ws/src/thesis_bringup/setup.py
+++ ros2_ws/src/thesis_bringup/setup.py
@@ -32,6 +32,7 @@ setup(
             "proposal_simulation_cell_v1_7_pre_control_contract_node = thesis_bringup.proposal_simulation_cell_v1_7_pre_control_contract_node:main",
             "proposal_simulation_cell_v1_8_control_scaffold_node = thesis_bringup.proposal_simulation_cell_v1_8_control_scaffold_node:main",
             "proposal_simulation_cell_v1_9_control_law_dry_run_node = thesis_bringup.proposal_simulation_cell_v1_9_control_law_dry_run_node:main",
+            "proposal_simulation_cell_v1_10_experiment_matrix_node = thesis_bringup.proposal_simulation_cell_v1_10_experiment_matrix_node:main",
         ]
     },
 )
